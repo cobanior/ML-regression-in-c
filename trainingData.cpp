efd600234a3f96e130e0c1cd2ddf0481ac2140bb
Orest Cobani <46853962+cobanior@users.noreply.github.com>
2019-11-15 12:50:09 -0500

    Add files via upload

---
+++ trainingData.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+#include "trainingData.h"
+
+void trainingData::set_x(float a)
+{
+    x = a; 
+}
+
+float trainingData::get_x(){
+    return x;
+}
+
+void trainingData::set_y(float a){
+    y = a;
+}
+
+float trainingData::get_y(){
+    return y;
+}
---
+++ trainingData.h
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+    * File: trainingData.h
+ * Author: orest
+ *
+ * Created on November 14, 2019, 5:06 PM
+ */
+
+#include <iostream>
+
+#ifndef TRAININGDATA_H
+#define TRAININGDATA_H
+class trainingData {
+private:
+    float x;
+    float y;
+public:
+    //function prototypes
+    void set_x(float );
+    float get_x();
+    void set_y(float );
+    float get_y();
+};
+
+#endif /* TRAININGDATA_H */
+
